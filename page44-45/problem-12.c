1b0e039039e90722bfcaa2edc73409b461c9be83
apesoda <43443669+apesoda@users.noreply.github.com>
2024-10-12 22:22:04 +0200

    Worked on problem 12

--- page44-45/problem-12.c
+++ page44-45/problem-12.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
 int main(){
-  
-};
+  char name[13], dob[10], tel[16];
+  printf("Welcome to the profile intro generator!\nPlease enter your first name: ");
+  scanf("%s", name);
+  printf("Please enter your date of birth (DD-MM-YYYY): ");
+  scanf("%s", dob);
+  printf("Please enter your mobile number: ");
+  scanf("%s", tel);
+  printf("\nHi everyone!\nMy name is %s, I was born on %s.\nYou can reach me by dialing %s", name, dob, tel);
+  return 0;
+}
