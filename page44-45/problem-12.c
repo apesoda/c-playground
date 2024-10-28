d50cde7c0972c38e3b1df88a5333c93a359eed20
apesoda <43443669+apesoda@users.noreply.github.com>
2024-10-28 22:06:17 +0100

    Still works using magic, but ironed out a big brain fart

--- page44-45/problem-12.c
+++ page44-45/problem-12.c
@@ -3,12 +3,12 @@
 int main(){
   char name[13], dob[10], tel[16];
   printf("Welcome to the profile intro generator!\nPlease enter your first name: ");
-  scanf("%s", name);
+  scanf("%s", &name);
   printf("Please enter your date of birth (DD-MM-YYYY): ");
-  scanf("%s", dob);
+  scanf("%s", &dob);
   printf("Please enter your mobile number: ");
-  scanf("%s", tel);
-  printf("\nHi everyone!\nMy name is %s, I was born on %s.\nYou can reach me by dialing %s", name, dob, tel);
+  scanf("%s", &tel);
+  printf("\nHi everyone!\nMy name is %s, I was born on %s.\nYou can reach me by dialing %s \n", name, dob, tel);
   return 0;
 }
 // this is really ugly and there is a bug that makes it so the name only shows up under magic circumstances
