3ebf513505b8da8ea18f2ac4eeefbbfb24ce44e0
apesoda <43443669+apesoda@users.noreply.github.com>
2024-11-01 21:15:41 +0100

    Fixed up problem-22

--- page44-45/problem-22.c
+++ page44-45/problem-22.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 int main(){
-  double num1, num2, num3, sum, avg;
+  int num1, num2, num3;
+  float sum, avg;
   printf("Provide the first number: ");
-  scanf("%0.1f", &num1);
+  scanf("%d", &num1);
   printf("Provide the second number: ");
-  scanf("%0.1f", &num2);
+  scanf("%d", &num2);
   printf("Provide the third number: ");
-  scanf("%0.1f", &num3);
+  scanf("%d", &num3);
   sum = num1 + num2 + num3;
   avg = sum / 3.0;
-  printf("The average between %0.1f, %0.1f and %0.1f is %0.1f \n", num1, num2, num3, avg);
+  printf("The average between %d, %d and %d is %0.1f \n", num1, num2, num3, avg);
   return 0;
 }
