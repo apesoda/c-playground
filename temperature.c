fa3e07231f23ef695535111d306d027c5df50c16
apesoda <43443669+apesoda@users.noreply.github.com>
2024-11-08 21:08:28 +0100

    added logic (file is still broken)

--- temperature.c
+++ temperature.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main() {
-  int
+  int cel, fah, opt;
+  printf("## CONVERT YOUR TEMPS ##\n(1) C->F\n(2) F->C\nConvert from Celsius to Fahrenheit?(1) Or from Fahrenheit to Celsius?(2): ");
+  scanf("%d", &opt);
+  if (opt == 1) {
+    printf("Enter your temperature in C: \n");
+    scanf("%d", &cel);
+    fah = cel * (9/5) + 32;
+    // printf("option 1");
+  }
+  else if (opt == 2) {
+    printf("option 2");
+  }
+  else {
+    printf("no\n");
+  };
+  return 0;
 }
