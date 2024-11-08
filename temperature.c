42f522a094ed6c5237d508abad6f558795b80491
apesoda <43443669+apesoda@users.noreply.github.com>
2024-11-08 21:34:21 +0100

    Converting to Fahrenheit works now

--- temperature.c
+++ temperature.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  int cel, fah, opt;
+  int opt;
+  float cel, fah;
   printf("## CONVERT YOUR TEMPS ##\n(1) C->F\n(2) F->C\nConvert from Celsius to Fahrenheit?(1) Or from Fahrenheit to Celsius?(2): ");
   scanf("%d", &opt);
   if (opt == 1) {
-    printf("Enter your temperature in C: \n");
-    scanf("%d", &cel);
-    fah = cel * (9/5) + 32;
-    // printf("option 1");
+    printf("Enter your temperature in C: ");
+    scanf("%f", &cel);
+    fah = cel * (9.0/5.0) + 32;
+    printf("%0.1f degrees Celsius is %0.1f degrees Fahrenheit \n", cel, fah);
   }
   else if (opt == 2) {
     printf("option 2");
