9f9ece23ca97206b91902794dae6b7be0adee590
apesoda <43443669+apesoda@users.noreply.github.com>
2024-11-08 22:07:24 +0100

    Laying the groundwork for more options in the little menu

--- temperature.c
+++ temperature.c
@@ -3,7 +3,7 @@
 int main() {
   int opt;
   float cel, fah;
-  printf("## CONVERT YOUR TEMPS ##\n(1) C->F\n(2) F->C\nConvert from Celsius to Fahrenheit?(1) Or from Fahrenheit to Celsius?(2): ");
+  printf("## CONVERT YOUR TEMPS ##\n(1) C->F\n(2) F->C\n(q) Quit\nConvert from Celsius to Fahrenheit?(1) Or from Fahrenheit to Celsius?(2): ");
   scanf("%d", &opt);
   if (opt == 1) {
     printf("Enter your temperature in C: ");
@@ -16,6 +16,9 @@ int main() {
     scanf("%f", &fah);
     cel = (fah - 32) * (5.0/9.0);
     printf("%0.1f degrees Fahrenheit is %0.1f degrees Celsius \n", fah, cel);
+  }
+  else if (opt = 'q') {
+    return 0;
   }
   else {
     printf("no\n");
