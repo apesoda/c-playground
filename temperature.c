6f89d4e334f051dfcd328ce2dd999412d0db12fd
apesoda <43443669+apesoda@users.noreply.github.com>
2024-11-09 18:43:40 +0100

    All menu options work now

--- temperature.c
+++ temperature.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int main() {
-  int opt;
+  char opt;
   float cel, fah;
   printf("## CONVERT YOUR TEMPS ##\n(1) C->F\n(2) F->C\n(q) Quit\nConvert from Celsius to Fahrenheit?(1) Or from Fahrenheit to Celsius?(2): ");
-  scanf("%d", &opt);
-  if (opt == 1) {
+  scanf("%c", &opt);
+  if (opt == '1') {
     printf("Enter your temperature in C: ");
     scanf("%f", &cel);
     fah = cel * (9.0/5.0) + 32;
     printf("%0.1f degrees Celsius is %0.1f degrees Fahrenheit \n", cel, fah);
   }
-  else if (opt == 2) {
+  else if (opt == '2') {
     printf("Enter your temperature in F: ");
     scanf("%f", &fah);
     cel = (fah - 32) * (5.0/9.0);
     printf("%0.1f degrees Fahrenheit is %0.1f degrees Celsius \n", fah, cel);
   }
-  else if (opt = 'q') {
+  else if (opt == 'q') {
     return 0;
   }
   else {
