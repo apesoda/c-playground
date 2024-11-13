d69538b1afe0a40e829e3cea9d4126e863116cf4
apesoda <43443669+apesoda@users.noreply.github.com>
2024-11-13 20:17:07 +0100

    added start for guessing game

---
+++ guess.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+/* int random input;
+ * random = <random 1-100>
+ * printf("Please enter a number: ");
+ * scanf("%d",&input);
+ * if (input == random){
+ * printf("You guessed it!\n");
+ * } else {
+ * printf("Oops, wrong!\n");
+ * };
+ * report 0;
+ * */
+}
