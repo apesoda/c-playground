dbefbc7693d13484539cf912bbfd9fd5de70a6e9
apesoda <43443669+apesoda@users.noreply.github.com>
2024-11-16 18:10:13 +0100

    You are now in an infinite loop until you actually guess the number

--- guess.c
+++ guess.c
@@ -3,13 +3,15 @@
 int main(){
   int random;
   int input;
-  random = rand() % 100 + 1;
-  printf("Please enter a number between 1 and 100: ");
-  scanf("%d",&input);
-  if (input == random){
-    printf("You guessed it!\n");
-  } else {
-    printf("Oops, wrong!\n");
-  };
+  random = rand() % (100 - 1 + 1) + 1;
+  do {
+    printf("Please enter a number between 1 and 100: ");
+    scanf("%d",&input);
+    if (input != random) {
+      printf("Nope, not it!\n");
+    }
+  }
+  while (input != random);
+  printf("That's it!\n");
   return 0;
 }
