635e69d80a0dd3873fb74f545c5b97575817e7d2
apesoda <43443669+apesoda@users.noreply.github.com>
2024-11-16 17:34:36 +0100

    Finished rudimentary version of the guessing game

--- guess.c
+++ guess.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(){
-/* int random input;
- * random = <random 1-100>
- * printf("Please enter a number: ");
- * scanf("%d",&input);
- * if (input == random){
- * printf("You guessed it!\n");
- * } else {
- * printf("Oops, wrong!\n");
- * };
- * report 0;
- * */
+  int random;
+  int input;
+  random = rand() % 1 + 1;
+  printf("Please enter a number between 1 and 100: ");
+  scanf("%d",&input);
+  if (input == random){
+    printf("You guessed it!\n");
+  } else {
+    printf("Oops, wrong!\n");
+  };
+  return 0;
 }
-
-/* ADD: While loop until guessed
- * ADD: Give up function
- * ADD: Game modes
