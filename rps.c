897af5d3be43eab7f657a75b0b21679ae7afc0ae
apesoda <43443669+apesoda@users.noreply.github.com>
2024-11-19 23:50:33 +0100

    started working on plotting out the logic for rps

--- rps.c
+++ rps.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 int main(){
-  /*
-   * 
-   *
-   * */
+  /* char cpu_pick, usr_pick;
+   * srand(time(NULL)); 
+   * printf("ROCK (r). PAPER (p). SCISSORS (s).\n");
+   */
 }
