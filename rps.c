5c9801e63513588810862681ec5f30129ff0cb63
apesoda <43443669+apesoda@users.noreply.github.com>
2025-01-16 17:16:30 +0100

    added base functionality, will remove debug print statements later

--- rps.c
+++ rps.c
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 int main(){
-  /* char cpu_pick, usr_pick;
-   * srand(time(NULL)); 
-   * cpu_pick = "rpc"[rand() % 3];
-   * printf("ROCK (r). PAPER (p). SCISSORS (s).\n");
-   */
+  char cpu_pick, usr_pick;
+  srand(time(NULL)); 
+  cpu_pick = "rps"[rand() % 3];
+  printf("ROCK (r) PAPER (p) SCISSORS (s)\n");
+  printf("%c", cpu_pick);
+  scanf("%c", &usr_pick);
+  if (usr_pick == cpu_pick) {
+    printf("Tie-breaker! \n");
+  }
+  else {
+    printf("Not a tie\n");
+  }
+  return 0;
 }
